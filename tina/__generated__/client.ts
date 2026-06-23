c035baf9a54707bd4e268053f4fb1f9616668498
Jewitch-Dev <294007194+Jewitch-Dev@users.noreply.github.com>
2026-06-23 21:45:52 +0000

    Build Neato site

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,2 +1,5 @@
-export default ()=>({})
-export const client = ()=>({})
+import { createClient } from "tinacms/dist/client";
+import { queries } from "./types";
+export const client = createClient({ cacheDir: '/home/runner/work/jewitch/jewitch/tina/__generated__/.cache/1782251122125', url: 'https://content.tinajs.io/1.6/content/10f49e2b-5c36-4a1a-be72-268088c0f3da/github/main', token: '9caba6f47ed7059182dc25af60132d743cbc7386', queries,  });
+export default client;
+  
---
+++ tina/__generated__/config.prebuild.jsx
@@ -0,0 +1,99 @@
+// tina/config.ts
+import { defineConfig } from "tinacms";
+var branch = process.env.HEAD || process.env.GITHUB_REF_NAME || process.env.CF_PAGES_BRANCH || "main";
+var config_default = defineConfig({
+  branch,
+  clientId: "10f49e2b-5c36-4a1a-be72-268088c0f3da",
+  token: process.env.TINA_TOKEN,
+  build: {
+    outputFolder: "admin",
+    publicFolder: "published"
+  },
+  media: {
+    tina: {
+      mediaRoot: "assets",
+      publicFolder: "content"
+    }
+  },
+  schema: {
+    collections: [
+      {
+        name: "posts",
+        label: "Posts",
+        path: "content/posts",
+        format: "md",
+        ui: {
+          filename: {
+            readonly: false,
+            slugify: (values) => {
+              const title = values?.title || "new-post";
+              return String(title).toLowerCase().replace(/[^a-z0-9]+/g, "-").replace(/^-|-$/g, "");
+            }
+          }
+        },
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true
+          },
+          {
+            type: "string",
+            name: "description",
+            label: "Description",
+            ui: {
+              component: "textarea"
+            }
+          },
+          {
+            type: "datetime",
+            name: "created",
+            label: "Created",
+            required: true,
+            ui: {
+              dateFormat: "YYYY-MM-DD",
+              timeFormat: "HH:mm:ss"
+            }
+          },
+          {
+            type: "datetime",
+            name: "modified",
+            label: "Modified",
+            ui: {
+              dateFormat: "YYYY-MM-DD",
+              timeFormat: "HH:mm:ss"
+            }
+          },
+          {
+            type: "string",
+            name: "tags",
+            label: "Tags",
+            description: "Comma-separated, like: Mania, Creativity, Personal"
+          },
+          {
+            type: "string",
+            name: "template",
+            label: "Template",
+            required: true
+          },
+          {
+            type: "string",
+            name: "uuid",
+            label: "UUID"
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Post",
+            isBody: true
+          }
+        ]
+      }
+    ]
+  }
+});
+export {
+  config_default as default
+};
